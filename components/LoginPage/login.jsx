220de8e2e7b25e7a384784b5b22adb055d2f9573
Akilesh <akileshakileshs1234@gmail.com>
2025-04-29 10:18:31 +0530

    Included the react-native-responsive-screen module to make the UI responsive for different sizes and changed the styles sheet accordingly.

--- components/LoginPage/login.jsx
+++ components/LoginPage/login.jsx
@@ -10,6 +10,10 @@ import {
   Image,
   //   CheckBox,
 } from "react-native";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
 
 const loginPageBackground = require("../../assets/LoginPage/loginPageBackground.png");
 const loginRevozenLogo = require("../../assets/LoginPage/loginRenozenLogo.png");
@@ -106,98 +110,96 @@ const LoginScreen = () => {
 
 const styles = StyleSheet.create({
   backgroundImage: {
-    marginTop: 100,
-    width: "100%",
-    height: "100%",
+    marginTop: hp("12%"),
+    width: wp("100%"),
+    height: hp("100%"),
   },
   overlay: {
-    // backgroundColor: "rgba(0,0,0,0.4)",
-    marginTop: 20,
+    marginTop: hp("2%"),
     justifyContent: "center",
     alignItems: "center",
-    borderRadius: 15,
+    borderRadius: wp("4%"),
     borderWidth: 2,
-    borderColor: '#cccccc',
-    marginHorizontal: 15,
-    padding: 15
+    borderColor: "#cccccc",
+    marginHorizontal: wp("4%"),
+    padding: wp("4%"),
   },
   revozenLogo: {
-    width: 170,
-    height: 100,
-    marginTop: 140,
-    marginLeft: 120,
+    width: wp("40%"),
+    height: hp("12%"),
+    marginTop: hp("14%"),
+    marginLeft: wp("30%"),
   },
   form: {
-    // backgroundColor: "#fff",
-    width: "85%",
-    borderRadius: 20,
+    width: wp("85%"),
+    borderRadius: wp("5%"),
   },
   flexColumn: {
-    marginBottom: 5,
+    marginBottom: hp("1%"),
   },
   label: {
     color: "#151717",
     fontWeight: "600",
-    fontSize: 16,
+    fontSize: wp("4%"),
   },
   inputForm: {
     flexDirection: "row",
     alignItems: "center",
     borderWidth: 1.5,
     borderColor: "#ecedec",
-    borderRadius: 10,
-    height: 50,
-    marginBottom: 15,
-    paddingLeft: 10,
+    borderRadius: wp("3%"),
+    height: hp("6%"),
+    marginBottom: hp("2%"),
+    paddingLeft: wp("3%"),
   },
   iconPlaceholder: {
-    width: 20,
-    height: 20,
+    width: wp("5%"),
+    height: wp("5%"),
     backgroundColor: "#ccc",
-    borderRadius: 5,
+    borderRadius: wp("2%"),
   },
   input: {
     flex: 1,
-    marginLeft: 10,
-    fontSize: 16,
+    marginLeft: wp("2%"),
+    fontSize: wp("4%"),
     color: "#000",
   },
   flexRow: {
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
-    marginVertical: 15,
+    marginVertical: hp("2%"),
   },
   rememberMeContainer: {
     flexDirection: "row",
     alignItems: "center",
   },
   rememberMeText: {
-    fontSize: 14,
+    fontSize: wp("3.5%"),
     color: "#000",
   },
   forgotText: {
-    fontSize: 14,
+    fontSize: wp("3.5%"),
     color: "#2d79f3",
     fontWeight: "500",
   },
   buttonSubmit: {
     backgroundColor: "#151717",
-    paddingVertical: 15,
-    borderRadius: 10,
-    marginTop: 10,
+    paddingVertical: hp("2%"),
+    borderRadius: wp("3%"),
+    marginTop: hp("1%"),
   },
   buttonSubmitText: {
     color: "#fff",
-    fontSize: 16,
+    fontSize: wp("4%"),
     textAlign: "center",
     fontWeight: "600",
   },
   pText: {
     textAlign: "center",
-    fontSize: 14,
+    fontSize: wp("3.5%"),
     color: "#000",
-    marginTop: 10,
+    marginTop: hp("2%"),
   },
   spanText: {
     color: "#2d79f3",
@@ -205,10 +207,12 @@ const styles = StyleSheet.create({
   },
   socialButton: {
     flex: 1,
-    paddingVertical: 12,
-    borderRadius: 10,
+    paddingVertical: hp("1.5%"),
+    borderRadius: wp("3%"),
     alignItems: "center",
-    marginHorizontal: 5,
+    marginHorizontal: wp("1.5%"),
+    flexDirection: "row",
+    justifyContent: "center",
   },
   google: {
     backgroundColor: "#fff",
@@ -220,8 +224,13 @@ const styles = StyleSheet.create({
   },
   socialButtonText: {
     color: "#000",
-    fontSize: 16,
+    fontSize: wp("4%"),
     fontWeight: "600",
+    marginLeft: wp("2%"),
+  },
+  icon: {
+    width: wp("5%"),
+    height: wp("5%"),
   },
 });
 
